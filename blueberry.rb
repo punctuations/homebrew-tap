9cc9f32066e509c14db786a9048c5b579f48aac4
matt <matt@dont-ping.me>
2022-06-13 17:51:01 -0700

    fix: update sha256

--- blueberry.rb
+++ blueberry.rb
@@ -7,8 +7,8 @@ class Blueberry < Formula
   version "1.0.0"
 
   on_macos do
-    url "https://github.com/punctuations/blueberry/releases/download/v1.0.0-canary/blueberry-macos-latest.tar.gz"
-    sha256 "8aa93a7fa9718933ac4762dd44d627819184950c2999faa4c4c826e611b6962d"
+    url "https://github.com/punctuations/blueberry/releases/download/v1.0.0/blueberry-macos-latest.tar.gz"
+    sha256 "ecec2f2a4b46184550edbcadfae5980f0df79413f967ad649bf20dfd18f20e7b"
 
     def install
       bin.install "blueberry"
@@ -16,8 +16,8 @@ class Blueberry < Formula
   end
 
   on_linux do
-    url "https://github.com/punctuations/blueberry/releases/download/v1.0.0-canary/blueberry-ubuntu-latest.tar.gz"
-    sha256 "ffcb13b9551c923c9fa37864dbdbb64656be7b40572fb648e0edba9c0731f57a"
+    url "https://github.com/punctuations/blueberry/releases/download/v1.0.0/blueberry-ubuntu-latest.tar.gz"
+    sha256 "e013357b83d9c988fea2ae8cfc77cd6828d16570dd74c4efe0620377cb2ed9c6"
 
     def install
       bin.install "blueberry"
