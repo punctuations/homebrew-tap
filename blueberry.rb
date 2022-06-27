67bf241fc6882583bd51fa60e230751bcc6e1675
punctuations <matt@dont-ping.me>
2022-06-27 06:31:18 +0000

    ci: bump blueberry to v1.0.4

--- blueberry.rb
+++ blueberry.rb
@@ -4,11 +4,11 @@
 class Blueberry < Formula
   desc "🫐  A command-line tool for command-line tools."
   homepage "https://github.com/punctuations/blueberry"
-  version "v1.0.3"
+  version "v1.0.4"
 
   on_macos do
-    url "https://github.com/punctuations/blueberry/releases/download/v1.0.3/blueberry-macos-latest.tar.gz"
-    sha256 "346fc9ddb747beccfed961227195502c0ec30a708a2922ec61e9c22298d5"
+    url "https://github.com/punctuations/blueberry/releases/download/v1.0.4/blueberry-macos-latest.tar.gz"
+    sha256 "6e8a9d86ebff9265ddce3d4403650713e28b98edadd0bd3894d033e9414b"
 
     def install
       bin.install "blueberry"
@@ -16,8 +16,8 @@ class Blueberry < Formula
   end
 
   on_linux do
-    url "https://github.com/punctuations/blueberry/releases/download/v1.0.3/blueberry-ubuntu-latest.tar.gz"
-    sha256 "22efed0535b861991f2e9d82c3f1c3c127019194ccad057a7da77e650ef3"
+    url "https://github.com/punctuations/blueberry/releases/download/v1.0.4/blueberry-ubuntu-latest.tar.gz"
+    sha256 "21db2b27066304d3e74a4ba4c7e089cd17930346e34888c5740366a1810e"
 
     def install
       bin.install "blueberry"
