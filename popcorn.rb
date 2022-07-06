677b3f4e7f93b3975d9b61eed5ffe7772e5d8b38
punctuations <matt@dont-ping.me>
2022-07-06 23:27:23 +0000

    ci: bump popcorn to v1.0.6

--- popcorn.rb
+++ popcorn.rb
@@ -8,7 +8,7 @@ class Popcorn < Formula
 
   on_macos do
     url "https://github.com/punctuations/popcorn/releases/download/v1.0.6/popcorn-macos-latest.tar.gz"
-    sha256 "7146ab3f7931956ae095b8e815b43b69bc06323a31b6631e95286b2955a4"
+    sha256 "ce2e9dd28735d214eaeacb384e8a45f5ae2a290b1161cad0e8352433b0b7"
 
     def install
       bin.install "popcorn"
@@ -17,7 +17,7 @@ class Popcorn < Formula
 
   on_linux do
     url "https://github.com/punctuations/popcorn/releases/download/v1.0.6/popcorn-ubuntu-latest.tar.gz"
-    sha256 "4296bf46d9db1a5fa984d3eed60c72b9ccf59333b148551762d8874e482b"
+    sha256 "c0722eb67f639214e8da02d1d02d46e381dc8a6ee1b8af1003b208120ab0"
 
     def install
       bin.install "popcorn"
